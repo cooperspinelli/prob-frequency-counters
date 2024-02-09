e5aedc5ffafc43ada9be7afe926f2d2e9925bd48
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-02-09 15:43:31 -0800

    average-pair passed the test

--- average-pair.js
+++ average-pair.js
@@ -1,4 +1,22 @@
 // add whatever parameters you deem necessary & write docstring
-function averagePair() {
+//input : sorted arr and num
+// output: boolean
+// use multiple pointer
+// return true if there is an average
+function averagePair(sortedNums, aveNum) {
+  let left = 0;
+  let right = sortedNums.length - 1;
+
+  while (left < right) {
+    const average = (sortedNums[left] + sortedNums[right]) / 2;
+    if (average === aveNum) {
+      return true;
+    } else if (average > aveNum) {
+      right--;
+    } else {
+      left++;
+    }
+  }
+  return false;
 }
 
