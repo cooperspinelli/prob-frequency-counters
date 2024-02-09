6c17715f3802d7ed53e80c473e427476a1902a4a
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-09 16:33:10 -0700

    Write can-construct-word, passing tests

--- can-construct-word.js
+++ can-construct-word.js
@@ -1,3 +1,25 @@
 // add whatever parameters you deem necessary & write doc comment
-function canConstructWord() {
+
+function freqCounter(string) {
+  let counter = {};
+
+  for (let char of string) {
+    counter[char] = counter[char] ? counter[char] + 1 : 1;
+  }
+  return counter;
+}
+
+function canConstructWord(word, letters) {
+
+  if (word.length > letters.length) return false;
+
+  let wordFreqCounter = freqCounter(word);
+  let letterFreqCounter = freqCounter(letters);
+
+  for (let char in wordFreqCounter) {
+    if (letterFreqCounter[char] === undefined) return false;
+    if (wordFreqCounter[char] > letterFreqCounter[char]) return false;
+  }
+
+  return true;
 }
