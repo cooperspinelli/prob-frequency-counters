57c692fb481e61786b8761c64703c4e1f7409451
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-02-09 16:50:33 -0800

    separatePositive and isSubsequence's solution added

--- is-subsequence.js
+++ is-subsequence.js
@@ -1,3 +1,18 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function isSubsequence() {
+/** Takes two strings and returns a boolean value whether the characters
+ * in the s1 appear somewhere in the s2, without their order changing. */
+
+function isSubsequence(s1, s2) {
+  let i = 0;
+  let j = 0;
+
+  while (j < s2.length) {
+    if (s1[i] === s2[j]) {
+      i++;
+    }
+    j++;
+  }
+  return i === s1.length;
 }
--- separate-positive.js
+++ separate-positive.js
@@ -1,3 +1,25 @@
+"use strict";
+
 // add whatever parameters you deem necessary & write docstring
-function separatePositive() {
+
+/** Takes a list of numbers and returns the same array but positive nums
+ * will be on left and negative numbers will be on right. It does not have
+ * to be sorted.
+ */
+function separatePositive(nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    if (nums[left] < 0 && nums[right] > 0) {
+      [nums[left],nums[right]] = [nums[right],nums[left]];
+    }
+    if (nums[left] > 0) {
+      left++;
+    }
+    if (nums[right] < 0) {
+      right--;
+    }
+  }
+  return nums;
 }
