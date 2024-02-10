71163317363da32c32fc82b5f79bf6b13820881a
Cooper Spinelli <cooperspinelli@gmail.com>
2024-02-09 17:17:11 -0700

    Write same-frequency and two-array-object, passing tests

--- same-frequency.js
+++ same-frequency.js
@@ -1,3 +1,35 @@
-// add whatever parameters you deem necessary & write docstring
-function sameFrequency() {
+"use strict";
+
+
+/** Creates frequency counter for the characters of the input string
+ *
+ * Returns an object with keys represeting characters in the input string
+ * and values represeting the number of times that the character appears int
+ * the input string
+ */
+
+function freqCounter(string) {
+  let counter = {};
+
+  for (let char of string) {
+    counter[char] = counter[char] ? counter[char] + 1 : 1;
+  }
+  return counter;
+}
+
+/** Takes two positive integers and checks if they have the same
+ * frequency of digits
+ *
+ * Returns a boolean
+ */
+
+function sameFrequency(num1, num2) {
+  const num1Counter = freqCounter(num1.toString());
+  const num2Counter = freqCounter(num2.toString());
+
+  for (let key in num1Counter) {
+    if (num1Counter[key] !== num2Counter[key]) return false;
+  }
+
+  return true;
 }
--- two-array-object.js
+++ two-array-object.js
@@ -1,3 +1,23 @@
-// add whatever parameters you deem necessary & write docstring
-function twoArrayObject() {
+"use strict";
+
+/** Creates an object from two arrays, uses the first array as keys
+ * and the second array as values. If key array is longer than value array,
+ * overflow keys are assigned the value of null. If value array is longer
+ * than key array, overflow values are ignored.
+ *
+ * Assuming that all elements of key array are unique
+ *
+ * Returns object constructed from two arrays
+ */
+
+function twoArrayObject(keys, values) {
+
+  const constructedFromArrays = {};
+
+  for (let i = 0; i < keys.length; i++) {
+    const value = i < values.length ? values[i] : null;
+    constructedFromArrays[keys[i]] = value;
+  }
+
+  return constructedFromArrays;
 }
